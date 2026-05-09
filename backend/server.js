c42c566aad58ac1fdaf965fd854718d1796588c3
Abdur Rahman <119003720+siyamar@users.noreply.github.com>
2026-05-09 17:08:20 +0600

    change port

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5006;
 
 let MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI && process.env.DB_USER && process.env.DB_PASS) {
